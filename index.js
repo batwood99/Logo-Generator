9aea49d6f7e06d2ca364a688d686d1f71b68a64d
batwood99 <atwood@mail.usf.edu>
2023-05-01 11:40:15 -0400

    created prompt

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 const svgCaptcha = require('svg-captcha');
 const fs = require('fs');
+
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for your logo:'
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (color keyword or hexadecimal number):'
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape for your logo:',
+      choices: ['circle', 'triangle', 'square']
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (color keyword or hexadecimal number):'
+    }
